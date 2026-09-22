3cf62ae006d763fb2a0796bd1162531257e96cce
Shannon Smith <infiltratr@yandex.com>
2026-09-22 20:23:01 +1000

    Make EXT reservation hardening comment-independent

--- tools/harden-ext-ranges.py
+++ tools/harden-ext-ranges.py
@@ -147,24 +147,22 @@ replace_once(
 )
 replace_once(
     ROOT / "ext2/kernel/balloc.c",
-    """\twhile (1) {
-\t\tif (cur <= rsv->rsv_end)
+    """\t\tif (cur <= rsv->rsv_end)
 \t\t\tcur = rsv->rsv_end + 1;
-
-
-\t\tif (cur > last_block)
-\t\t\treturn -1;
 """,
-    """\twhile (1) {
-\t\tif (cur <= rsv->rsv_end) {
+    """\t\tif (cur <= rsv->rsv_end) {
 \t\t\tif (check_add_overflow(rsv->rsv_end, (ext2_fsblk_t)1,
 \t\t\t\t       &cur))
 \t\t\t\treturn -1;
 \t\t}
-
-\t\tif (cur > last_block || size == 0 ||
+""",
+)
+replace_once(
+    ROOT / "ext2/kernel/balloc.c",
+    """\t\tif (cur > last_block)
+""",
+    """\t\tif (cur > last_block || size == 0 ||
 \t\t    check_add_overflow(cur, (ext2_fsblk_t)(size - 1), &end))
-\t\t\treturn -1;
 """,
 )
 replace_once(
@@ -193,24 +191,22 @@ replace_once(
 )
 replace_once(
     ROOT / "ext3/kernel/balloc.c",
-    """\twhile (1) {
-\t\tif (cur <= rsv->rsv_end)
+    """\t\tif (cur <= rsv->rsv_end)
 \t\t\tcur = rsv->rsv_end + 1;
-
-
-\t\tif (cur > last_block)
-\t\t\treturn -1;
 """,
-    """\twhile (1) {
-\t\tif (cur <= rsv->rsv_end) {
+    """\t\tif (cur <= rsv->rsv_end) {
 \t\t\tif (check_add_overflow(rsv->rsv_end, (ext3_fsblk_t)1,
 \t\t\t\t       &cur))
 \t\t\t\treturn -1;
 \t\t}
-
-\t\tif (cur > last_block || size == 0 ||
+""",
+)
+replace_once(
+    ROOT / "ext3/kernel/balloc.c",
+    """\t\tif (cur > last_block)
+""",
+    """\t\tif (cur > last_block || size == 0 ||
 \t\t    check_add_overflow(cur, (ext3_fsblk_t)(size - 1), &end))
-\t\t\treturn -1;
 """,
 )
 replace_once(
