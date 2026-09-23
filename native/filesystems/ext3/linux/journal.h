426c0276ba238af158593709d91669c66ff9d7ba
Shannon Smith <infiltratr@yandex.com>
2026-09-23 12:13:04 +1000

    Internalize removed EXT3 JBD buffer locking API

--- native/filesystems/ext3/linux/journal.h
+++ native/filesystems/ext3/linux/journal.h
@@ -47,6 +47,7 @@
 
 #include <linux/types.h>
 #include <linux/buffer_head.h>
+#include <linux/bit_spinlock.h>
 #include <linux/journal-head.h>
 #include <linux/stddef.h>
 #include <linux/mutex.h>
@@ -264,7 +265,50 @@ BUFFER_FNS(RevokeValid, revokevalid)
 TAS_BUFFER_FNS(RevokeValid, revokevalid)
 BUFFER_FNS(Freed, freed)
 
-#include <linux/jbd_common.h>
+/*
+ * Linux removed the standalone JBD common header after EXT3 itself was
+ * retired.  Keep the original JBD locking contract local to this embedded
+ * journal engine instead of depending on JBD2.
+ */
+static inline struct buffer_head *jh2bh(struct journal_head *jh)
+{
+	return jh->b_bh;
+}
+
+static inline struct journal_head *bh2jh(struct buffer_head *bh)
+{
+	return bh->b_private;
+}
+
+static inline void jbd_lock_bh_state(struct buffer_head *bh)
+{
+	bit_spin_lock(BH_State, &bh->b_state);
+}
+
+static inline int jbd_trylock_bh_state(struct buffer_head *bh)
+{
+	return bit_spin_trylock(BH_State, &bh->b_state);
+}
+
+static inline int jbd_is_locked_bh_state(struct buffer_head *bh)
+{
+	return bit_spin_is_locked(BH_State, &bh->b_state);
+}
+
+static inline void jbd_unlock_bh_state(struct buffer_head *bh)
+{
+	bit_spin_unlock(BH_State, &bh->b_state);
+}
+
+static inline void jbd_lock_bh_journal_head(struct buffer_head *bh)
+{
+	bit_spin_lock(BH_JournalHead, &bh->b_state);
+}
+
+static inline void jbd_unlock_bh_journal_head(struct buffer_head *bh)
+{
+	bit_spin_unlock(BH_JournalHead, &bh->b_state);
+}
 
 #define J_ASSERT(assert)	BUG_ON(!(assert))
 
