634682431dab154e5767a42bf23f5af9cc51db14
Shannon Smith <infiltratr@yandex.com>
2026-09-23 12:17:31 +1000

    Modernize EXT3 Linux wrapper compatibility

--- native/filesystems/ext3/linux/jbd_journal.c
+++ native/filesystems/ext3/linux/jbd_journal.c
@@ -1742,7 +1742,7 @@ void journal_ack_err(journal_t *journal)
  */
 int journal_blocks_per_page(struct inode *inode)
 {
-	return 1 << (PAGE_CACHE_SHIFT - inode->i_sb->s_blocksize_bits);
+	return 1 << (PAGE_SHIFT - inode->i_sb->s_blocksize_bits);
 }
 
 
