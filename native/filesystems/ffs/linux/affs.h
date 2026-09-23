b916edf689863784d371ef490854ea68dc1ade84
Shannon Smith <infiltratr@yandex.com>
2026-09-23 19:16:54 +1000

    Harden OFS and FFS Linux buffer allocation

--- native/filesystems/ffs/linux/affs.h
+++ native/filesystems/ffs/linux/affs.h
@@ -254,8 +254,10 @@ affs_getzeroblk(struct super_block *sb, int block)
 	pr_debug("%s: %d\n", __func__, block);
 	if (affs_validblock(sb, block)) {
 		bh = sb_getblk(sb, block);
+		if (!bh)
+			return NULL;
 		lock_buffer(bh);
-		memset(bh->b_data, 0 , sb->s_blocksize);
+		memset(bh->b_data, 0, sb->s_blocksize);
 		set_buffer_uptodate(bh);
 		unlock_buffer(bh);
 		return bh;
@@ -269,6 +271,8 @@ affs_getemptyblk(struct super_block *sb, int block)
 	pr_debug("%s: %d\n", __func__, block);
 	if (affs_validblock(sb, block)) {
 		bh = sb_getblk(sb, block);
+		if (!bh)
+			return NULL;
 		wait_on_buffer(bh);
 		set_buffer_uptodate(bh);
 		return bh;
