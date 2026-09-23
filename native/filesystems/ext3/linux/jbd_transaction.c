53ebec15b3006e2d6fbe6d0eeda320bfd8e81217
Shannon Smith <infiltratr@yandex.com>
2026-09-23 13:51:28 +1000

    Modernize EXT3 JBD device diagnostics

--- native/filesystems/ext3/linux/jbd_transaction.c
+++ native/filesystems/ext3/linux/jbd_transaction.c
@@ -450,13 +450,11 @@ void journal_unlock_updates (journal_t *journal)
  */
 static void warn_dirty_buffer(struct buffer_head *bh)
 {
-	char b[BDEVNAME_SIZE];
-
 	printk(KERN_WARNING
-	       "JBD: Spotted dirty metadata buffer (dev = %s, blocknr = %llu). "
+	       "JBD: Spotted dirty metadata buffer (dev = %pg, blocknr = %llu). "
 	       "There's a risk of filesystem corruption in case of system "
 	       "crash.\n",
-	       bdevname(bh->b_bdev, b), (unsigned long long)bh->b_blocknr);
+	       bh->b_bdev, (unsigned long long)bh->b_blocknr);
 }
 
 
