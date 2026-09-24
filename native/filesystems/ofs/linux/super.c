6ab79479b663e054a2b7c59c6e0d043b08873819
Shannon Smith <infiltratr@yandex.com>
2026-09-24 12:25:20 +1000

    Restore required OFS module metadata

--- native/filesystems/ofs/linux/super.c
+++ native/filesystems/ofs/linux/super.c
@@ -787,6 +787,7 @@ static void __exit ifs_ofs_exit_fs(void)
 }
 
 MODULE_DESCRIPTION(IFS_OFS_MODULE_DESCRIPTION);
+MODULE_LICENSE("Proprietary");
 
 module_init(ifs_ofs_init_fs)
 module_exit(ifs_ofs_exit_fs)
