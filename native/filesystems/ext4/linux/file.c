c8c6b5c8a928412eecac4f7c1625c3773e8374ec
Shannon Smith <infiltratr@yandex.com>
2026-09-24 15:02:16 +1000

    Order EXT4 fsync data before metadata durability

--- native/filesystems/ext4/linux/file.c
+++ native/filesystems/ext4/linux/file.c
@@ -1189,6 +1189,10 @@ int ext4_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 		goto out;
 	}
 
+	ret = file_write_and_wait_range(file, start, end);
+	if (ret)
+		goto out;
+
 	if (!EXT4_SB(inode->i_sb)->s_journal) {
 		ret = ext4_fsync_nojournal(file, start, end, datasync,
 					   &needs_barrier);
@@ -1196,11 +1200,6 @@ int ext4_sync_file(struct file *file, loff_t start, loff_t end, int datasync)
 			goto issue_flush;
 		goto out;
 	}
-
-	ret = file_write_and_wait_range(file, start, end);
-	if (ret)
-		goto out;
-
 
 	ret = ext4_fsync_journal(inode, datasync, &needs_barrier);
 
