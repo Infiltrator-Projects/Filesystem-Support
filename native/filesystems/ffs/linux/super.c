dbc9e1f4687af38439492d91122bf1756b668377
Shannon Smith <infiltratr@yandex.com>
2026-09-24 11:44:35 +1000

    Remove FFS inherited licence declaration

--- native/filesystems/ffs/linux/super.c
+++ native/filesystems/ffs/linux/super.c
@@ -787,7 +787,6 @@ static void __exit ifs_ffs_exit_fs(void)
 }
 
 MODULE_DESCRIPTION(IFS_FFS_MODULE_DESCRIPTION);
-MODULE_LICENSE("GPL");
 
 module_init(ifs_ffs_init_fs)
 module_exit(ifs_ffs_exit_fs)
